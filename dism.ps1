cd387169b49e47c3244cd6fda11852e8806c4c86
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2022-04-25 23:24:21 +0700

    Update dism.ps1

--- dism.ps1
+++ dism.ps1
@@ -1,4 +1,9 @@
+#Lấy thông tin Windows
 Get-WindowsImage -ImagePath D:\WinWork\ISO\install.wim
+#Chọn phiên bản Windows cần thêm driver
 Mount-WindowsImage -Path D:\WinWork\Mount\ -ImagePath D:\WinWork\ISO\install.wim -Index 2
+#Thêm driver vào phiên bản windows đã chọn
 Add-WindowsDriver -Path D:\WinWork\Mount\ -Driver D:\DriverBackup -Recurse
+#Lưu các thay đổi vào file .wim
 Dismount-WindowsImage -Path D:\WinWork\Mount\ –Save
+
